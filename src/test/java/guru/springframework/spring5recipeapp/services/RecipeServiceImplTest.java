3ef9013cc58e224ff816df6f61550c2dfb754812
Nitish Singh <nsingh07@syr.edu>
2020-06-01 23:16:40 -0400

    Added test and implementation for FindById function for recipe.

--- src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -9,9 +9,13 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 class RecipeServiceImplTest {
 
@@ -27,6 +31,21 @@ class RecipeServiceImplTest {
     }
 
     @Test
+    void getRecipeByIdTest(){
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        Mockito.when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull(recipeReturned, "Null Recipe Returned");
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
     void getRecipes() {
         Recipe recipe = new Recipe();
         HashSet<Recipe> recipeData = new HashSet<>();
@@ -38,6 +57,6 @@ class RecipeServiceImplTest {
 
         assertEquals(recipes.size(), 1);
 
-        Mockito.verify(recipeRepository, Mockito.times(1)).findAll();
+        verify(recipeRepository, Mockito.times(1)).findAll();
     }
 }
