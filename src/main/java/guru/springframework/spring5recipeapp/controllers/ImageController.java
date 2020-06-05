12c383ec54063ce9996fb3bcaa3cf048ad6ade84
Nitish Singh <nsingh07@syr.edu>
2020-06-05 17:50:02 -0400

    Changed the image controller

--- src/main/java/guru/springframework/spring5recipeapp/controllers/ImageController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/ImageController.java
@@ -2,14 +2,16 @@ package guru.springframework.spring5recipeapp.controllers;
 
 import guru.springframework.spring5recipeapp.services.ImageService;
 import guru.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Controller
 public class ImageController {
     private final ImageService imageService;
@@ -20,10 +22,11 @@ public class ImageController {
         this.recipeService = recipeService;
     }
 
-    @GetMapping("/recipe/{recipeId}/image")
+    @RequestMapping("/recipe/{recipeId}/image")
     public String showUploadForm(@PathVariable String recipeId, Model model){
+        log.info("Inside showUploadForm Controller");
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
-        return "recipe/imageform";
+        return "recipe/imageuploadform";
     }
 
     @PostMapping("/recipe/{recipeId}/image")
